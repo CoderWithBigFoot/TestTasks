c5d0d02f53ac58d9adfabc446a972a95fc8e749c
Zheka <korsakasPr@gmail.com>
2017-01-27 18:07:51 +0300

    The first commit

---
+++ Airport(The task to check myself)/Airport(The task to check myself)/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Airport_The_task_to_check_myself_
+{
+   public class Program
+    {
+       public static void Main(string[] args)
+        {
+
+            Console.ReadKey();
+        }
+    }
+}
